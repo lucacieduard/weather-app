6cf85020f0548e364ac001759d7beb5facc13326
lucacieduard <eduardlucaci2002@yahoo.com>
2023-04-12 20:28:19 +0300

    home page fix

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,17 @@
 import classes from "./home.module.css";
 import NavBar from "../../components/navbar/NavBar";
 import Sidebar from "../../components/sidebar/Sidebar";
+import MainCity from "../../components/mainCity/mainCity";
+import PageContainer from "../../UI/PageContainer/PageCointainer";
 const Home = () => {
 	return (
 		<div className={classes.page}>
 			<Sidebar />
 			<div className={classes.container}>
 				<NavBar />
+				<PageContainer>
+					<MainCity />
+				</PageContainer>
 			</div>
 		</div>
 	);
