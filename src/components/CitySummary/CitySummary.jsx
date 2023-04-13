e5dc9c701f7feafe74662c9630372eaa6728023c
lucacieduard <eduardlucaci2002@yahoo.com>
2023-04-13 15:58:20 +0300

    citySummary update

---
+++ src/components/CitySummary/CitySummary.jsx
@@ -0,0 +1,89 @@
+import Parameter from "./Parameter/Parameter";
+import classes from "./citySummary.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+	faTemperatureLow,
+	faWind,
+	faCompass,
+	faArrowRight,
+} from "@fortawesome/free-solid-svg-icons";
+import MapC from "../Map/Map";
+
+const CitySummary = (props) => {
+	const coords = {
+		București: {
+			lat: 44.43225,
+			lng: 26.10626,
+		},
+		"Cluj Napoca": { lat: 46.770439, lng: 23.591423 },
+		Brașov: {
+			lat: 45.657975,
+			lng: 25.601198,
+		},
+		Iași: {
+			lat: 47.151726,
+			lng: 27.587914,
+		},
+		Sibiu: {
+			lat: 45.792784,
+			lng: 24.152069,
+		},
+	};
+	return (
+		<div className={classes.summary_container}>
+			<div className={classes.summary}>
+				<div className={classes.info}>
+					<h2 className={classes.cityName}>{props.city}</h2>
+					<p className={classes.lastUpdate}>
+						Ultima actualizare : 13/04/2023 13:34
+					</p>
+				</div>
+				<div className={classes.summary_parameters}>
+					<Parameter
+						type="Temperatura"
+						value="20"
+						unit="°C"
+						icon={
+							<FontAwesomeIcon
+								icon={faTemperatureLow}
+								style={{ width: "30px", height: "30px" }}
+							/>
+						}
+					/>
+					<Parameter
+						type="Viteza vantului"
+						value="18"
+						unit="km/h"
+						icon={
+							<FontAwesomeIcon
+								icon={faWind}
+								style={{ width: "30px", height: "30px" }}
+							/>
+						}
+					/>
+					<Parameter
+						type="Directia vantului"
+						value="300"
+						unit="°"
+						icon={
+							<FontAwesomeIcon
+								icon={faCompass}
+								style={{ width: "30px", height: "30px" }}
+							/>
+						}
+					/>
+					<p className={classes.codeMessage}>Status : Code message</p>
+					<button className={classes.button}>
+						<span>Vremea detaliata {props.city}</span>
+						<FontAwesomeIcon icon={faArrowRight} />
+					</button>
+				</div>
+			</div>
+			<div className={classes.map}>
+				<MapC coords={coords[props.city]} />
+			</div>
+		</div>
+	);
+};
+
+export default CitySummary;
