48febbab5b0ecd08b2b2d1dae6fdee05b0ec5738
lucacieduard <eduardlucaci2002@yahoo.com>
2023-04-13 15:57:40 +0300

    CardCity update

--- src/components/mainCity/CardCity.jsx
+++ src/components/mainCity/CardCity.jsx
@@ -2,7 +2,11 @@ import classes from "./cardCity.module.css";
 
 const CardCity = (props) => {
 	return (
-		<div className={classes.card}>
+		<div
+			className={`${classes.card} ${
+				props.active === props.cityName ? `${classes.active}` : ""
+			}`}
+		>
 			<img className={classes.card_image} src={props.image} />
 			<p className={classes.card_title}>{props.cityName}</p>
 		</div>
