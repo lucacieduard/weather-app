22f8234fc6b7eafba01db62ec72369464246bab0
lucacieduard <eduardlucaci2002@yahoo.com>
2023-04-13 18:44:47 +0300

    scroll on map

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -12,7 +12,7 @@ const MapC = (props) => {
 		<MapContainer
 			center={[`${props.coords.lat}`, `${props.coords.lng}`]}
 			zoom={12}
-			scrollWheelZoom={false}
+			scrollWheelZoom={true}
 			style={{ width: "100%", height: "100%" }}
 		>
 			<TileLayer
