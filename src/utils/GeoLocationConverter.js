19830aeb3b5b4b2b73dbdf74a54c68fda8d69347
lucacieduard <eduardlucaci2002@yahoo.com>
2023-04-15 16:59:36 +0300

    new helper functions

---
+++ src/utils/GeoLocationConverter.js
@@ -0,0 +1,14 @@
+export default async function GeoConvertor(cityName) {
+	const response = await fetch(
+		`https://geocoding-api.open-meteo.com/v1/search?name=${cityName}&count=1&language=en&format=json`
+	);
+
+	const { results } = await response.json();
+	const city = results[0].name;
+
+	const lat = results[0].latitude;
+	const lng = results[0].longitude;
+	const countryCode = results[0].country_code;
+
+	return { city, lat, lng, countryCode };
+}
---
+++ src/utils/coords.js
@@ -0,0 +1,41 @@
+const coords = {
+	București: {
+		lat: 44.43225,
+		lng: 26.10626,
+	},
+	"Cluj-Napoca": { lat: 46.770439, lng: 23.591423 },
+	Brașov: {
+		lat: 45.657975,
+		lng: 25.601198,
+	},
+	Iași: {
+		lat: 47.151726,
+		lng: 27.587914,
+	},
+	Sibiu: {
+		lat: 45.792784,
+		lng: 24.152069,
+	},
+	Brussels: {
+		lat: 50.85045,
+		lng: 4.34878,
+	},
+	Rome: {
+		lat: 41.902782,
+		lng: 12.496366,
+	},
+	Paris: {
+		lat: 48.856613,
+		lng: 2.352222,
+	},
+	Madrid: {
+		lat: 40.416775,
+		lng: -3.70379,
+	},
+	Budapest: {
+		lat: 47.497913,
+		lng: 19.040236,
+	},
+};
+
+export default coords;
